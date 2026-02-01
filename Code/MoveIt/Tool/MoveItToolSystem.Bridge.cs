0037dba79947f37c8aa8551ff613d5766176af48
yenyang <tjkelley19@gmail.com>
2026-02-01 09:23:26 -0800

    changes to static bridge methods

--- Code/MoveIt/Tool/MoveItToolSystem.Bridge.cs
+++ Code/MoveIt/Tool/MoveItToolSystem.Bridge.cs
@@ -12,28 +12,27 @@ namespace MoveIt.Tool
 {
     public partial class MoveItToolSystem : ObjectToolBaseSystem
     {
-        public static bool TryGetOriginalEntity(Entity entity,  out Entity original)
+        public static Entity GetOriginalEntity(Entity entity)
         {
             if (entity != Entity.Null &&
                 m_Instance.EntityManager.TryGetComponent(entity, out MIT_Original mit_original) &&
                 mit_original.m_Original != Entity.Null)
             {
-                original = mit_original.m_Original;
-                return true;
+                return mit_original.m_Original;
             }
 
-            original = Entity.Null;
-            return false;
+            return Entity.Null;
         }
 
         public static EntityQuery GetMIT_OriginalQueryWith(ComponentType[] all, ComponentType[] any, ComponentType[] none)
         {
-            ComponentType[] allPlusMITOriginal = new ComponentType[all.Length + 1];
+            ComponentType[] allPlusMITOriginal = new ComponentType[all.Length + 2];
             for (int i = 0; i < all.Length; i++)
             {
                 allPlusMITOriginal[i] = all[i];
             }
             allPlusMITOriginal[all.Length] = ComponentType.ReadOnly<MIT_Original>();
+            allPlusMITOriginal[all.Length+1] = ComponentType.ReadOnly<Game.Tools.Temp>();
 
             return m_Instance.GetEntityQuery(new EntityQueryDesc[] { new EntityQueryDesc { All = allPlusMITOriginal, Any = any, None = none } });
         }
